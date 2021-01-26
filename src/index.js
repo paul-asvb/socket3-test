2e39eb9427c73191d460cf554b9f0efd6f97c265
Paul Mayer <paul@paul-mayer.eu>
2021-01-26 21:16:55 +0100

    add npm start cmd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 const app = require('express')();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/static/send.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+    console.log('chat message', msg);
+  });
+  socket.on('ping', function () {
+    socket.emit('pong');
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('ping', function () {
+    socket.emit('pong');
+  });
 });
 
 http.listen(3000, () => {
